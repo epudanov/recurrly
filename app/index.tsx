117421668fbfb5e7c3cd023df2a8083a1db623c9
Evgenii Pudanov <evgenii.pudanov@gmail.com>
2026-05-27 21:59:28 -0500

    Add tabs navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,18 @@
-import "@/styles/global.css";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-export default function App() {
+export default function Onboarding() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Welcome to Onboarding!
       </Text>
+      <Link
+        href="/(tabs)/subscriptions"
+        className="mt-4 px-4 py-2 bg-blue-500 rounded"
+      >
+        Go to Tabs
+      </Link>
     </View>
   );
 }
