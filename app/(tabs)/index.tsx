c9721cc9dff34ccbd69ddb661c89a36e48175ab9
Evgenii Pudanov <evgenii.pudanov@gmail.com>
2026-06-02 21:02:21 -0500

    Add custom fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ const SafeAreaView = styled(SafeAreaViewBase);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold">Index Screen</Text>
+      <Text className="text-5xl font-sans-extrabold">Index Screen</Text>
     </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded, fontsError] = useFonts({
+    Sans: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Sans-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Sans-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "Sans-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Sans-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "Sans-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontsError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontsError]);
+
+  if (!fontsLoaded && !fontsError) {
+    return null;
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
