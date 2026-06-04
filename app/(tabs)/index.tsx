42e0c0a319229137fb3d644984ca42c447ee66bb
Evgenii Pudanov <evgenii.pudanov@gmail.com>
2026-06-03 21:17:11 -0500

    Add homepage user profile header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,9 @@
+import { HOME_USER } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
 import "@/styles/global.css";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView as SafeAreaViewBase } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(SafeAreaViewBase);
@@ -8,7 +11,14 @@ const SafeAreaView = styled(SafeAreaViewBase);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Text className="text-5xl font-sans-extrabold">Index Screen</Text>
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar" />
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
+
+        <Image source={icons.add} className="home-add-icon" />
+      </View>
     </SafeAreaView>
   );
 }
